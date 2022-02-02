b4d78b1d92eeeca95072b7a39aaa75902a0a5828
James Henrique <jamesrpm@hotmail.com>
2022-02-02 08:50:56 -0300

    Sessao do Usuario, criaçao do  modal de consulta dados pessoais

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 window.onload = ()=>{
-
     if (document.getElementById('show-password')) {
         let showPasswordButton = document.getElementById('show-password');
         let passwordInput = document.getElementById('password');
@@ -18,4 +17,36 @@ window.onload = ()=>{
             }
         })
     }
+    
+    function deleteCookie(name){
+        let date = new Date(2000,0,01);
+        date = date.toUTCString();
+        document.cookie = name +'=; expires=' + date + ';path=/';
+        window.location.href = '/login.html';
+    
+    }
+
+    if (!getCookie('authorization') && location.href != 'http://127.0.0.1:5500/login.html') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Sessão Expirada!',
+            text: 'Por favor, entre com suas credenciais',
+            showCloseButton: false,
+            confirmButtonText: 'OK'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = '/login.html';
+            }
+          })
+        
+    }
+
+    if (document.getElementById('exit')){
+        let exitLink = document.getElementById('exit');
+        exitLink.addEventListener('click', ()=> {
+            deleteCookie('authorization');
+        })
+    }
+
+  
 }
--- assets/js/requests.js
+++ assets/js/requests.js
@@ -1,6 +1,23 @@
 const baseUrl = 'http://localhost:8080/api/';
 
-    
+function createCookie(value){
+    let date = new Date();
+    date.setMinutes(date.getMinutes() + 10);
+    date = date.toUTCString();
+    document.cookie = 'authorization='+ value + '; expires=' + date + ';path=/';    
+}
+
+function getCookie(name) {
+    var cookieName = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(cookieName) == 0) return c.substring(cookieName.length, c.length);
+    }
+    return null;
+}
+   
 if (document.querySelector('#signup')){
     const signUp = document.querySelector('#signup');
 
@@ -8,7 +25,7 @@ if (document.querySelector('#signup')){
         e.preventDefault();
         let data = new FormData(signUp);
         let json = JSON.stringify(Object.fromEntries(data));
-        console.log(data.getAll);
+        // console.log(data.getAll);
         axios({
             method: 'post',
             url: baseUrl + 'clients/create',
@@ -16,7 +33,7 @@ if (document.querySelector('#signup')){
             data: json,
         })
         .then((response)=>{
-            console.log(response);
+            // console.log(response);
             window.location.href = '/login.html';
         })
         .catch((err) => {
@@ -36,7 +53,7 @@ if (document.querySelector('#signin')){
         e.preventDefault();
         let data = new FormData(signIn);
         let json = JSON.stringify(Object.fromEntries(data));
-        console.log(data.getAll);
+        // console.log(data.getAll);
         axios({
             method: 'post',
             url: 'http://localhost:8080/login',
@@ -44,9 +61,10 @@ if (document.querySelector('#signin')){
             data: json,
         })
         .then((response)=>{
-            console.log(response);
-            document.cookie = 'authorization='+ response.headers.authorization;
+            // console.log(response);
+            createCookie(response.headers.authorization);
             window.location.href = '/';
+
         })
         .catch((err) => {
             Swal.fire({
@@ -57,4 +75,39 @@ if (document.querySelector('#signin')){
         });
     });
 }
+
+let auth = getCookie('authorization');
+let dataPersonLink = document.getElementById('dataPersonLink');
+
+if (dataPersonLink){
+    dataPersonLink.addEventListener('click', ()=>{
+        axios({
+            method: 'get',
+            url: baseUrl + 'clients',
+            headers: (
+                {'Content-Type': 'application/json'},
+                {'Authorization': auth}
+            ),
+            
+        })
+        .then((response)=>{
+            document.getElementById('fullName').value = response.data.fullName;
+            document.getElementById('income').value = response.data.income;
+            document.getElementById('email').value = response.data.email;
+            document.getElementById('cpf').value = response.data.cpf;
+            document.getElementById('rg').value = response.data.rg;
+            document.getElementById('fullAddress').value = response.data.fullAddress;
+        })
+        .catch((err) => {
+            window.location.href = '/';
+            console.log(err);
+    
+        });
+    }) 
+}
+
+   
+
+
+
     
